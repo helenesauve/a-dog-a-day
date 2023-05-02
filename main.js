7068dceeefbca5c4b7b769e6e2c47363bf2ffc7d
Helene <helenesauveparis@gmail.com>
2023-05-02 21:19:00 +0100

    api call to giphy and rough page set up

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+$("#dog-button").on("click", function() {
+
+    // emptying current dog gif
+    $("#images").html("");
+
+// Storing our giphy API URL for a random dog image
+
+let queryUrl = "https://api.giphy.com/v1/gifs/random?api_key=" + apiKey + "&tag=dog&rating=pg";
+
+
+  // Perfoming an AJAX GET request to our queryURL
+  $.ajax({
+    url: queryUrl,
+    method: "GET"
+  })
+
+      // After the data from the AJAX request comes back
+      .then(function(response) {
+         // Saving the image_original_url property
+         var imageUrl = response.data.images.original.url;
+
+          // Creating and storing an image tag
+          var dogImage = $("<img>");
+
+          // Setting the catImage src attribute to imageUrl
+          dogImage.attr("src", imageUrl);
+          dogImage.attr("alt", "dog image");
+
+          // Prepending the catImage to the images div
+          $("#images").prepend(dogImage);
+        });
+    });
